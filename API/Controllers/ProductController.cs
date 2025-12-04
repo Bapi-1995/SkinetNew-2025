e532d9128c5fd904bed92f282dcfa71e39b5c84c
rahuldutta.dutta688 <rahuldutta.dutta688@gmail.com>
2025-12-04 20:30:40 +0530

    Section 3

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 using System;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,24 +9,19 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductController : ControllerBase
+public class ProductController(IProductRepository repo) : ControllerBase
 {
-    private readonly StoreContext storeContext;
-
-    public ProductController(StoreContext storeContext)
-    {
-        this.storeContext = storeContext;
-    }
+   
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts(string? brand, string? type,string? sort)
     {
-        var products = await storeContext.Products.ToListAsync();
+        var products = await repo.GetProductsAsync(brand, type,sort);
         return Ok(products);
     }
     [HttpGet("{id:int}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        var product = await storeContext.Products.FindAsync(id);
+        var product = await repo.GetProductByIdAsync(id);
         if (product == null)
         {
             return NotFound();
@@ -35,9 +31,12 @@ public class ProductController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<Product>> CreateProduct(Product product)
     {
-        storeContext.Products.Add(product);
-        await storeContext.SaveChangesAsync();
-        return product;
+        repo.AddProduct(product);
+        if(await repo.SaveChangesAsync())
+        {
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+        return BadRequest("Failed to create product");
         //CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
     }
     [HttpPut("{id:int}")]
@@ -47,39 +46,44 @@ public class ProductController : ControllerBase
         {
             return BadRequest("Cannot update this product");
         }
-        storeContext.Entry(product).State = EntityState.Modified;
-        try
-        {
-            await storeContext.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-            if (!ProductExists(id))
-            {
-                return NotFound();
-            }
-            else
-            {
-                throw;
-            }
-        }
-        return NoContent();
+        
+             repo.UpdateProduct(product);
+             if(await repo.SaveChangesAsync())
+             {
+                return NoContent();
+             }
+        return BadRequest("Failed to update product");
     }
     [HttpDelete("{id:int}")]
     public async Task<ActionResult> DeleteProduct(int id)
     {
-        var product = await storeContext.Products.FindAsync(id);
+        var product = await repo.GetProductByIdAsync(id);
         if (product == null)
         {
             return NotFound();
         }
-        storeContext.Products.Remove(product);
-        await storeContext.SaveChangesAsync();
-        return NoContent();
+        repo.DeleteProduct(product);
+        if(await repo.SaveChangesAsync())
+             {
+                return NoContent();
+             }
+        return BadRequest("Failed to delete product");
+    }
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+    {
+        
+        return Ok(await repo.GetBrandsAsync());
+    }
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+    {
+        
+        return Ok(await repo.GetTypesAsync());
     }
     private bool ProductExists(int id)
     {
-        return storeContext.Products.Any(e => e.Id == id);
+        return repo.ProductExists(id);
     }
 
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,6 +13,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
@@ -19,4 +21,20 @@ var app = builder.Build();
 
 app.MapControllers();
 
+try
+{
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+    Console.WriteLine(ex);
+    throw;
+}
+
 app.Run();
+
